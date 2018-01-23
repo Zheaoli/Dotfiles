a074cd00969430f3b9e0ec0e33983afdf221d60b
everettjf <everettjf@live.com>
2018-01-23 21:26:53 +0800

    amlazynoob

--- spacemacs_setup.sh
+++ spacemacs_setup.sh
@@ -2,9 +2,10 @@ rm -rf ~/.emacs.d
 rm ~/.spacemacs
 rm -rf ~/.spacmacs.d
 
-ln -s ~/github/Dotfiles/.spacemacs.d ~/.spacemacs.d
 
 git clone https://github.com/syl20bnr/spacemacs ~/.emacs.d
 
 emacs
+
+ln -s ~/github/Dotfiles/.spacemacs.d ~/.spacemacs.d
 
