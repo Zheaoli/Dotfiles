2e627b6e98bede125c047c28c90ff813ff228558
everettjf <everettjf@live.com>
2018-01-23 15:46:04 +0800

    amlazynoob

---
+++ fvim/config/setup.sh
@@ -0,0 +1,3 @@
+# flake for python
+rm ~/.config/flake8
+ln -s ~/.fvim/flake8 ~/.config/flake8
---
+++ fvim/setup.sh
@@ -0,0 +1,106 @@
+#!/usr/bin/env bash
+
+help() {
+    echo "setup.sh -- setup fvim"
+    echo "Usage: setup.sh -i|-u"
+    echo "-i -- install fvim"
+    echo "-u -- update fvim"
+    exit 0
+}
+
+color_print() {
+    printf '\033[0;31m%s\033[0m\n' "$1"
+}
+
+warn() {
+    color_print "$1" >&2
+}
+
+die() {
+    warn "$1"
+    exit 1
+}
+
+logo() {
+    color_print "Thank you for installing fvim!"
+    color_print ' _______     _____ __  __ '
+    color_print '|  ___\ \   / /_ _|  \/  |'
+    color_print '| |_   \ \ / / | || |\/| |'
+    color_print '|  _|   \ V /  | || |  | |'
+    color_print '|_|      \_/  |___|_|  |_|'
+    color_print '                          '
+}
+
+require() {
+    color_print "Checking requirements for fvim..."
+	color_print "Checking Vim version..."
+	vim --version | grep -E 7.[3-9]\|8.[0-9] || die "Your vim's version is too low!\nPlease download higher version(7.3+) from http://www.vim.org/download.php"
+    color_print "Checking if git exists..."
+    which git || die "No git installed!\nPlease install git from http://git-scm.com/downloads/"
+    color_print "Check if ctags exists..."
+    which ctags || warn "No ctags installed!\nPlease install ctags form http://ctags.sourceforge.net/ after fvim intallation!"
+}
+
+install() {
+    cd $HOME
+
+    #color_print "Cloning fvim..."
+    #rm -rf $HOME/.fvim
+    #git clone https://github.com/everettjf/fvim.git $HOME/.fvim
+
+    color_print "Setting fvim vimrc..."
+    ln -s $HOME/.fvim/vimrc $HOME/.vimrc
+
+    color_print "Installing vim-plug..."
+    #curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+    mkdir -p ~/.vim/autoload/
+    cp ~/.fvim/temp/plug.vim ~/.vim/autoload/plug.vim
+
+    color_print "Installing colortheme..."
+    git clone https://github.com/kristijanhusak/vim-hybrid-material.git $HOME/.vim/bundle/vim-hybrid-material
+    color_print "Installing plugins using vim-plug..."
+    vim +PlugUpdate +qal
+
+    color_print "Installing Python develop dependencies"
+    pip install flake8
+    pip install yapf
+
+    # config Python syntax checking (flake)
+    rm ~/.config/flake8
+    ln -s ~/.fvim/flake8 ~/.config/flake8
+
+    color_print "Notice: For Golang develop run :GoInstallBinaries in vim"
+
+    color_print "fvim has been installed. Just enjoy vimming!"
+}
+
+update() {
+    color_print "updating fvim..."
+    cd $HOME/.fvim
+    git pull origin master
+    color_print "updating plugins..."
+    if [ -e $HOME/.vimrc ]; then
+        vim +PlugClean! +PlugUpdate +qal
+    fi
+}
+
+if [ $# -ne 1 ]; then
+    help
+fi
+
+while getopts ":iu" opts; do
+    case $opts in
+        i)
+            logo
+            require
+            install
+            ;;
+        u)
+            update
+            ;;
+        :)
+            help;;
+        ?)
+            help;;
+    esac
+done
