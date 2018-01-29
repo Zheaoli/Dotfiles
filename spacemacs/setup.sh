1fc8a755a1da5384c0e4a4657b8855341c493c08
everettjf <everettjf@live.com>
2018-01-29 11:26:07 +0800

    amlazynoob

--- spacemacs/setup.sh
+++ spacemacs/setup.sh
@@ -3,7 +3,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 echo "Dir = ${DIR}"
 
 rm ~/.spacemacs
-rm -rf ~/.spacmacs.d
+rm -rf ~/.spacemacs.d
 rm -rf ~/.emacs.d
 
 ln -s $DIR/.spacemacs.d/ ~/.spacemacs.d
