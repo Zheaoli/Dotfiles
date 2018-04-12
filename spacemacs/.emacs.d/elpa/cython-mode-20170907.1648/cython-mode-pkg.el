43d7288251fc090887795d248b22ea4ce790ac7b
everettjf <everettjf@live.com>
2018-04-12 14:21:25 +0800

    remove spacemacs

--- spacemacs/.emacs.d/elpa/cython-mode-20170907.1648/cython-mode-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "cython-mode" "20170907.1648" "Major mode for editing Cython files" 'nil :commit "a4398a4eed261240be894f5263628648e28cae21")
--- spacemacs/.emacs.d/elpa/epl-20150517.433/epl-autoloads.el
+++
@@ -1,15 +0,0 @@
-;;; epl-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
-
-;;;### (autoloads nil nil ("epl.el") (23143 19087 89644 483000))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; epl-autoloads.el ends here
